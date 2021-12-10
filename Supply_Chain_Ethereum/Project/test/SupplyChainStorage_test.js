2a34e0d0fcf4cce627c7fb9a95d746f44c3b1c2d
Sourabh Mishra <Sourabh19278@iiitd.ac.in>
2021-12-10 21:51:04 +0530

    updated test files

--- Supply_Chain_Ethereum/Project/test/SupplyChainStorage_test.js
+++ Supply_Chain_Ethereum/Project/test/SupplyChainStorage_test.js
@@ -24,11 +24,11 @@ contract('SupplyChainStorage',(accounts)=>{
          await this.SupplyChainStorage.setUser(accounts[1],"Rahul Bhai","100","Bande uthana",true,{from:accounts[1]});
       });
 
-      it('Set user should not be called by unAuthorized caller', async ()=>{
+      it("Must fail as the calling person don't have the rights( Permision Denied )", async ()=>{
          await this.SupplyChainStorage.setUser(accounts[1],"Rahul Bhai","100","Bande uthana",true,{from:accounts[1]});
       });
 
-      it("Unauthorization of accounts[1] should be done and thus it should not able to call set user hence failing should be done",async ()=>{
+      it("Must fail as the calling person don't have the rights( Permision Denied )",async ()=>{
          await this.SupplyChainStorage.authorizeCaller(accounts[1]);
          await this.SupplyChainStorage.deAuthorizeCaller(accounts[1]);
          await this.SupplyChainStorage.setUser(accounts[1],"Rahul Bhai","100","Bande uthana",true,{from:accounts[1]});
@@ -59,74 +59,23 @@ contract('SupplyChainStorage',(accounts)=>{
          await this.SupplyChainStorage.setUser(accounts[1],"Rahul Bhai","100","Bande uthana",true,{from:accounts[1]});
       });
 
-      it('Checking set user should return true', async ()=>{
-         const res = await this.SupplyChainStorage.setUser(accounts[1],"Rahul Bhai","100","Bande uthana",true)
-         res.should.equal(true);
-      });
+      it('User should get added with correct details', async ()=>{
+         var SupplyChainStorageContract;
+         return SupplyChainStorage.deployed().then(function(instance) {
+            SupplyChainStorageContract = instance;
+            return instance.setUser(accounts[1], "Name","ContactNo","Role",true);
+        }).then(function() {
+           
+            return SupplyChainStorageContract.getUser.call(accounts[1]);
+        }).then(function(result) {
+           
+            assert.equal("Name", result[0], "Name wasn't properly added");
+            assert.equal("ContactNo", result[1], "ContactNo wasn't properly added");
+            assert.equal("Role", result[2], "Role wasn't properly added");
+            assert.equal(true, result[3], "IsActive wasn't properly added");
 
+         });
+     });
    });
 
-
-
 });
-   /*
-   describe('Changing Ownership',()=>{
-        it('Owner should change',async ()=>{
-          await this.Ownable.transferOwnership(accounts[1]);
-          const newOwner = await this.Ownable.owner();
-          newOwner.should.equal(accounts[1]);
-        })
-      
-        it("Chaning of owner should be done with same accounts",async ()=>{
-           await this.Ownable.transferOwnership(accounts[1]);
-          const newOwner = await this.Ownable.owner();
-          newOwner.should.equal(accounts[1]);
-          await this.Ownable.transferOwnership(accounts[0],{from:accounts[1]});
-          const Owner = await this.Ownable.owner();
-          Owner.should.equal(accounts[0]);
-        })
-
-        it("Must fail as the calling person don't have the rights( Permision Denied )",async ()=>{
-           await this.Ownable.transferOwnership(accounts[1]);
-            const newOwner = await this.Ownable.owner();
-            newOwner.should.equal(accounts[1]);
-            await this.Ownable.transferOwnership(accounts[0],{from:accounts[0]}) 
-         
-        })
-
-        it("Checking event results in changing owner",async ()=>{
-            let result =  await this.Ownable.transferOwnership(accounts[1]);
-            truffleAssert.prettyPrintEmittedEvents(result);
-            truffleAssert.eventEmitted(result, 'OwnershipTransferred', (event) =>{
-               return event.oldOwner == accounts[0] && event.newOwner == accounts[1];
-            });
-        })
-    })
-     
-   describe("Checking renounceOwnership" ,()=>{
-        it ("Checking event results in owner renouncing",async ()=>{
-            let result =  await this.Ownable.renounceOwnership();
-            truffleAssert.eventEmitted(result, 'OwnershipRenounced', (event) =>{
-               return event.oldOwner == accounts[0]
-            });
-        })
-
-        it("Ownwership renouncing should be done with same accounts",async ()=>{
-           await this.Ownable.renounceOwnership();
-        })
-
-         it("Must fail as the calling person don't have the rights( Permision Denied )",async ()=>{
-             await this.Ownable.renounceOwnership({from:accounts[1]});
-        })
-
-         it("Ownership must change", async () =>{
-            await this.Ownable.renounceOwnership();
-            const  name = await this.Ownable.owner();
-            name.should.not.equal(accounts[0]);
-         })
-
-   })
-
-
-   */
-
